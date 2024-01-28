0cd4668348241b3169025b21cbbf97754c06f1e4
FruityQC <odjickfelix@gmail.com>
2024-01-28 01:06:08 -0500

    Add project files.

---
+++ Patches/PlayerControlledBPatch.cs
@@ -0,0 +1,31 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using LethalCompanyInputUtils;
+using LethalCompanyInputUtils.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitysLethalMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControlledBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void ModifySprint(ref float ___sprintMeter, ref bool ___isSprinting, ref float ___sprintMultiplier)
+        {
+            // Infinite Sprint
+            ___sprintMeter = 1f;
+
+            // Faster Sprint when sprinting
+            float sprintMultiplier = FruitysModBase.configSprinting;
+            if (___isSprinting)
+            {
+                ___sprintMultiplier = sprintMultiplier;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using FruitysLethalMod.Patches;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitysLethalMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class FruitysModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Fruity.LethalMod";
+        private const string modName = "FruitysLethalMod";
+        private const string modVersion = "0.0.1";
+
+        public static ConfigEntry<float> configSprinting;
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static FruitysModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null) 
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Fruitys mod has loaded");
+    
+            harmony.PatchAll(typeof(FruitysModBase));
+            harmony.PatchAll(typeof(PlayerControlledBPatch));
+
+            configSprinting = Config.Bind("Sprinting", "SprintMultiplier", 2f, "The multiplier you want applied when sprinting."); //Category, Name, Value, Desc
+
+        }
+    }
+}
